298700533355defddc9ed7a5df3acc66989a316d
JangWoojun <woojun0107@naver.com>
2023-02-27 16:57:31 +0900

    Initial Commit

---
+++ Mind Control/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mind Control
+//
+//  Created by Jangwoojun on 2023/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

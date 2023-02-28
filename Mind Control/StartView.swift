4efdb80b1b716e357e31c4c8cfd724d5d57aae79
JangWoojun <woojun0107@naver.com>
2023-02-28 11:49:26 +0900

    feat: StartView와 StartViewController 연결

--- Mind Control/StartView.swift
+++ Mind Control/StartView.swift
@@ -1,20 +1,14 @@
-//
-//  StartView.swift
-//  Mind Control
-//
-//  Created by Jangwoojun on 2023/02/28.
-//
-
 import UIKit
 
 class StartView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 
 }
--- Mind Control/StartViewController.swift
+++ Mind Control/StartViewController.swift
@@ -1,29 +1,13 @@
-//
-//  StartViewController.swift
-//  Mind Control
-//
-//  Created by Jangwoojun on 2023/02/28.
-//
-
 import UIKit
 
 class StartViewController: UIViewController {
 
+    let startView = StartView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

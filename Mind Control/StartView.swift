30fee7828ebec1a7b301557f972d1658f57a3090
JangWoojun <woojun0107@naver.com>
2023-02-28 11:46:34 +0900

    feat: StartView 생성

---
+++ Mind Control/StartView.swift
@@ -0,0 +1,20 @@
+//
+//  StartView.swift
+//  Mind Control
+//
+//  Created by Jangwoojun on 2023/02/28.
+//
+
+import UIKit
+
+class StartView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

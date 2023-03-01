87ef04eaea871e0970cb992a69e1304ac70ef293
JangWoojun <woojun0107@naver.com>
2023-03-01 23:09:50 +0900

    feat: 이동 방식 변경

--- Mind Control/StartViewController.swift
+++ Mind Control/StartViewController.swift
@@ -10,8 +10,19 @@ class StartViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setButton()
+    }
+    
+    func setButton() {
+        let tapPlay = UITapGestureRecognizer(target: self, action: #selector(moveMainView))
+        startView.playImageView.addGestureRecognizer(tapPlay)
+        startView.playImageView.isUserInteractionEnabled = true
         
     }
     
-
+    @objc func moveMainView() {
+        let vc = ViewController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: false)
+    }
 }

accbb7e80ff9fc955b4d8fddf438f9ce9007ac65
JangWoojun <woojun0107@naver.com>
2023-03-02 21:56:14 +0900

    feat: ProgressBar 증가 기능 추가

--- Mind Control/ViewController.swift
+++ Mind Control/ViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    weak var timer: Timer?
+    var time = 0.0
+    
     let mainView = MainView()
     
     override func loadView() {
@@ -10,7 +13,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [self] _ in
+            if time == 1.0 {
+                timer?.invalidate()
+            } else {
+                time += 0.001
+                mainView.saiminprogress.setProgress(Float(time), animated: true)
+            }
+        })
     }
 
 
